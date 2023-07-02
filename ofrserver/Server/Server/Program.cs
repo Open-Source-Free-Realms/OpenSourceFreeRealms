7e497890a6722ea3ebee72f7ce2177a2397f48ac
Jacob I <24721341+iratenebrarum@users.noreply.github.com>
2023-07-02 06:41:48 -0500

    Return if configuration does not exist

--- ofrserver/Server/Server/Program.cs
+++ ofrserver/Server/Server/Program.cs
@@ -42,89 +42,86 @@ namespace Server
         {
             Servers = new Dictionary<string, SOEServer>();
 
-            bool createDefault = !File.Exists(Options.ConfigFile);
-            FileStream file = new FileStream(Options.ConfigFile, FileMode.OpenOrCreate);
+            FileStream file = new FileStream(Options.ConfigFile, FileMode.Open);
 
-            if (createDefault)
+            if (!File.Exists(Options.ConfigFile))
             {
-                StreamWriter writer = new StreamWriter(file);
-                // This creates an empty Config.json File
+                Console.WriteLine("No configuration exists!");
+                return;
             }
-            else
-            {
-                // Read the contents of the file
-                StreamReader reader = new StreamReader(file);
-                JArray rootArray = new JArray();
 
+            // Read the contents of the file
+            StreamReader reader = new StreamReader(file);
+            JArray rootArray = new JArray();
+
+            try
+            {
+                rootArray = JArray.Parse(reader.ReadToEnd());
+                reader.Close();
+            }
+            catch (JsonReaderException)
+            {
+                Console.WriteLine("Invalid configuration!");
+                return;
+            }
+
+            foreach (var server in rootArray.Children<JObject>())
+            {
+                // Check if this property is an Object
+                if (server.Type != JTokenType.Object)
+                {
+                    Console.WriteLine("Invalid configuration! Servers must be JSON Objects!");
+                    Environment.Exit(0);
+                }
+
+                // Make a new config
+                Dictionary<string, dynamic> serverConfig = new Dictionary<string, dynamic>();
+
+                // Go through the server properties
+                foreach (var propertyKeyval in server)
+                {
+                    string name = propertyKeyval.Key;
+                    JToken property = propertyKeyval.Value;
+
+                    if (property.Type == JTokenType.Object)
+                    {
+                        // We have a component configuration
+                        Dictionary<string, dynamic> componentConfig = new Dictionary<string, dynamic>();
+
+                        // Get the component settings
+                        foreach (var componentKeyval in (JObject)property)
+                        {
+                            componentConfig.Add(componentKeyval.Key, componentKeyval.Value.Value<object>());
+                        }
+
+                        // Add it to the server configuration
+                        serverConfig.Add(propertyKeyval.Key, componentConfig);
+                    }
+                    else if (property.Type == JTokenType.Array)
+                    {
+                        serverConfig.Add(name, property.ToObject<string[]>());
+                    }
+                    else
+                    {
+                        // Server value
+                        serverConfig.Add(name, property.Value<object>());
+                    }
+                }
+
+                // Setup a new SOEServer instance
+                SOEServer newServer = new SOEServer(serverConfig);
+                newServer.Run();
+
+                // Add the new server to our servers list
                 try
                 {
-                    rootArray = JArray.Parse(reader.ReadToEnd());
-                    reader.Close();
+                    string serverName = newServer.Configuration["Name"];
+                    Servers.Add(serverName, newServer);
                 }
-                catch (JsonReaderException)
+                catch (Exception)
                 {
-                    Console.WriteLine("Invalid configuration!");
-                    return;
-                }
-
-                foreach (var server in rootArray.Children<JObject>())
-                {
-                    // Check if this property is an Object
-                    if (server.Type != JTokenType.Object)
-                    {
-                        Console.WriteLine("Invalid configuration! Servers must be JSON Objects!");
-                        Environment.Exit(0);
-                    }
-
-                    // Make a new config
-                    Dictionary<string, dynamic> serverConfig = new Dictionary<string, dynamic>();
-                    
-                    // Go through the server properties
-                    foreach (var propertyKeyval in server)
-                    {
-                        string name = propertyKeyval.Key;
-                        JToken property = propertyKeyval.Value;
-
-                        if (property.Type == JTokenType.Object)
-                        {
-                            // We have a component configuration
-                            Dictionary<string, dynamic> componentConfig = new Dictionary<string, dynamic>();
-
-                            // Get the component settings
-                            foreach (var componentKeyval in (JObject)property)
-                            {
-                                componentConfig.Add(componentKeyval.Key, componentKeyval.Value.Value<object>());
-                            }
-
-                            // Add it to the server configuration
-                            serverConfig.Add(propertyKeyval.Key, componentConfig);
-                        }
-                        else if (property.Type == JTokenType.Array)
-                        {
-                            serverConfig.Add(name, property.ToObject<string[]>());
-                        }
-                        else
-                        {
-                            // Server value
-                            serverConfig.Add(name, property.Value<object>());
-                        }
-                    }
-
-                    // Setup a new SOEServer instance
-                    SOEServer newServer = new SOEServer(serverConfig);
-                    newServer.Run();
-
-                    // Add the new server to our servers list
-                    try
-                    {
-                        string serverName = newServer.Configuration["Name"];
-                        Servers.Add(serverName, newServer);
-                    }
-                    catch (Exception)
-                    {
-                        Console.WriteLine("Invalid configuration! Two servers cannot have the same name!");
-                        Environment.Exit(0);
-                    }
+                    Console.WriteLine("Invalid configuration! Two servers cannot have the same name!");
+                    Environment.Exit(0);
                 }
             }
         }
